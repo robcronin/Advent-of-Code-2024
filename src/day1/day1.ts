3671c46c0e03833909a1ce5168dab0bca6a95985
robcronin <croninro@tcd.ie>
2022-11-25 20:43:10 +0000

    Remove day 1 example

--- src/day1/day1.ts
+++
@@ -1,8 +0,0 @@
-export const calculateFuelForMass = (mass: number) => {
-  return Math.floor(mass / 3) - 2;
-};
-
-export const day1 = (input: number[]) => {
-  const fuel = input.map((m) => calculateFuelForMass(m));
-  return fuel.reduce((a, b) => a + b, 0);
-};
