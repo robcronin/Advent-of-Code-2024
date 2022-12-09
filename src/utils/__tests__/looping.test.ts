fb3408631a040f5649918ad6d2ec8c30d9f76924
robcronin <robc@theodo.co.uk>
2022-12-09 18:27:27 +0000

    Allow reverse range looping

--- src/utils/__tests__/looping.test.ts
+++ src/utils/__tests__/looping.test.ts
@@ -1,10 +1,16 @@
 import { range } from '../looping';
 
 describe('range', () => {
-    it('should produce a range array for one input', () => {
-        expect(range(3)).toEqual([0, 1, 2]);
-    });
-    it('should produce a range array for two inputs', () => {
-        expect(range(3, 5)).toEqual([3, 4]);
-    });
+  it('should produce a range array for one input', () => {
+    expect(range(3)).toEqual([0, 1, 2]);
+  });
+  it('should produce a range array for two inputs', () => {
+    expect(range(3, 5)).toEqual([3, 4]);
+  });
+  it('should produce a range array backwards', () => {
+    expect(range(5, 3)).toEqual([5, 4]);
+  });
+  it('should produce a range array backwards to 0', () => {
+    expect(range(4, 0)).toEqual([4, 3, 2, 1]);
+  });
 });
--- src/utils/looping.ts
+++ src/utils/looping.ts
@@ -1,6 +1,14 @@
 export const range = (x: number, y?: number): number[] => {
-    const start = y ? x : 0;
-    const end = y ? y : x;
-    const zeroRange = [...Array(end - start).keys()];
-    return y ? zeroRange.map((x) => x + start) : zeroRange;
+  const start = y !== undefined ? x : 0;
+  const end = y !== undefined ? y : x;
+  const rangeLength = Math.abs(end - start);
+  const zeroRange = [...Array(rangeLength).keys()];
+  if (y === undefined) {
+    return zeroRange;
+  } else {
+    if (start < end) {
+      return zeroRange.map((x) => x + start);
+    }
+    return zeroRange.reverse().map((x) => x + end + 1);
+  }
 };
