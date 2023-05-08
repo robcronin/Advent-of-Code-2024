4be30917b2a2e2cc7770d83ac3d620b584c6523b
robcronin <robc@theodo.co.uk>
2023-05-08 22:19:23 +0100

    Add min and max Arr helper

--- src/utils/array.ts
+++ src/utils/array.ts
@@ -3,3 +3,15 @@ export const countArr = <T>(arr: T[], countFn: (i: T) => boolean) =>
 
 export const sumArr = <T>(arr: T[], sumFn: (i: T, index: number) => number) =>
   arr.reduce((acc, i, index) => sumFn(i, index) + acc, 0);
+
+export const maxArr = <T>(arr: T[], maxFn: (i: T, index: number) => number) =>
+  arr.reduce((max, i, index) => {
+    const value = maxFn(i, index);
+    return value > max ? value : max;
+  }, Number.MIN_VALUE);
+
+export const minArr = <T>(arr: T[], minFn: (i: T, index: number) => number) =>
+  arr.reduce((min, i, index) => {
+    const value = minFn(i, index);
+    return value < min ? value : min;
+  }, Number.MAX_VALUE);
